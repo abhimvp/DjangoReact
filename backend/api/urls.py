d1f7daf8a7de4dfab0a6652b340fa9c69d3ba9d0
abhimvp <abhishekreddyboddu1@gmail.com>
2024-10-03 02:44:23 -0500

    Frontend and backend setup is done and working

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 # we link this to main url file
 urlpatterns = [
     path("notes/",views.NoteListCreateView.as_view(),name="note-list"), # we are using the class based view to handle the request
-    path("notes/delete/<int:pk>",views.NoteDeleteView.as_view(),name="delete-note"),
+    path("notes/delete/<int:pk>/",views.NoteDeleteView.as_view(),name="delete-note"),
 ]
